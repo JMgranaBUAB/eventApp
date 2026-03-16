604052ba96b9de9eb772e2e60f7309d7c5a7f86f
JMgranaBUAB <jmquesadamartin@gmail.com>
2026-03-16 11:08:06 +0100

    add init bbdd and close ddbb

--- src/main/java/org/mvc1/Main.java
+++ src/main/java/org/mvc1/Main.java
@@ -1,11 +1,15 @@
 package org.mvc1;
 
+import org.mvc1.config.DBManager;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
 
-
+        //probar connexion a BBDD
+        DBManager.initConnection();
+        DBManager.closeConnection();
 
     }
 }
---
+++ src/main/java/org/mvc1/config/DBManager.java
@@ -0,0 +1,37 @@
+package org.mvc1.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBManager {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/event_app";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    private static Connection connection;
+
+    public static Connection initConnection(){
+        try{
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Conexión a la BBDD exitosa");
+
+        } catch (SQLException exception) {
+            System.out.println("Conexion fallida : " + exception.getMessage());
+        }
+
+        return connection;
+    }
+
+    public static void closeConnection(){
+        try{
+            connection.close();
+            System.out.println("Conexión cerrada con Éxito");
+        } catch (SQLException exception) {
+            System.out.println("Error al cerrar la BBDD : " + exception.getMessage());
+        }
+    }
+
+
+}
